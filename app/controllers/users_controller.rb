e871bc5bf82a936d7520a4108c7600ec8e29c7c5
masutaka85 <tkhr.bk67msd@gmail.com>
2020-09-29 11:50:14 +0900

    ストロングパラメーターの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,12 @@ class UsersController < ApplicationController
     
   end
 
+  def create
+    User.create(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :email, :)
+  end
 end
