3d13a4002bbf4e5ecd036e0cb0a11fcaf8cc64d6
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-02 20:39:31 +0900

    itemsコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
   end
   
-  def new
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :text, :category_id, :status_id, :shipping_charges_id, :shipping_prefectures_id, :days_to_ship_id, :price, :user)
   end
 end
