bda706fee795e483b657e168afd6c032cefe6258
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-02 20:38:39 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_prefecture
+  belongs_to_active_hash :days_to_ship
+
+  belongs_to :user
 end
