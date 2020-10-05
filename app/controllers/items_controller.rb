beb721defbd28ee0390ee25603e1bea4082d680d
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-05 17:43:43 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :status_id, :shipping_charges_id, :shipping_prefectures_id, :days_to_ship_id, :price, :user)
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_charges_id, :shipping_prefectures_id, :days_to_ship_id, :price, :user)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_prefecture
   belongs_to_active_hash :days_to_ship
 
+  has_one_attached :image
+
   belongs_to :user
   with_options presence: true do
     validates :name
