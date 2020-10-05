985d9c59038131a902f7594e05da2c0260d80ba0
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-05 17:25:40 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :days_to_ship
 
   belongs_to :user
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :status_id
+    validates :shipping_charges_id
+    validates :shipping_prefectures_id
+    validates :days_to_ship_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 end
