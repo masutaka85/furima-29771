4e845ebc59465cb5559d4bc61e6cc8e31edf24ec
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-08 19:34:21 +0900

    商品詳細表示機能の実装
    モデル名にsがついておらず相違があったのでその修正を含む。

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
-  belongs_to_active_hash :shipping_charge
-  belongs_to_active_hash :shipping_prefecture
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_prefectures
   belongs_to_active_hash :days_to_ship
 
   has_one_attached :image
