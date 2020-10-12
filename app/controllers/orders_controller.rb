01dc48fcf4f83e95a85f7852eaadbfdbd02674ad
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-12 22:33:30 +0900

    ルーティングの設定、コントローラー・モデルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index]
+
+  def index
+    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_charges_id, :prefectures_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:format])
+  end
 end
