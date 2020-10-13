00d019b6f65a5a7a15cbf7449d226b02af86b8c9
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-13 18:49:57 +0900

    アソシエーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    validates :post_number
+    validates :prefectures_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
 end
