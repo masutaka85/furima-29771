b1676be193fe1a36e8f8efb59c22d62c55b4e9a8
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-14 18:21:57 +0900

    payjpの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,54 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :set_item, only: [:index]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_page
 
   def index
-    
+    @order = OrderAddress.new
+  end
+
+
+  def create
+    @order = OrderAddress.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
-  def item_params
-    params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :shipping_charges_id, :prefectures_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  def order_params
+    params.permit(:post_number, :prefectures_id, :city, :house_number, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def set_item
-    @item = Item.find(params[:format])
+    @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
+
+  def move_to_page
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+    if @item.order != nil 
+      redirect_to root_path
+    end
+  end
+
+
 end
