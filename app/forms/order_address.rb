d27501c1c0598e2d2018739190f0ef50fc760de0
masutaka85 <tkhr.bk67msd@gmail.com>
2020-10-14 18:38:10 +0900

    formobjectの設定

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -4,10 +4,10 @@ class OrderAddress
   attr_accessor :post_number, :prefectures_id, :city, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :post_number
+    validates :post_number, format: { with: /\A\d{3}-\d{4}\z/ }, length: { maximum: 8 }
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: {with: /\A0\d{9,10}\z/}, length: { maximum: 11 }
     validates :user_id
     validates :item_id
     validates :token
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
     validates :email, format: { with: /@.+/, message: '無効なアドレスです' }
